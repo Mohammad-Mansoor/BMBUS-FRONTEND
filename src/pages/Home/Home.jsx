e658f8092e65bab58e9d3e1dcddda0a6cd496b62
obaidahmadzai <obaidullahkhan206@gmail.com>
2025-03-06 03:22:35 +0430

    Refactor layout components and enhance Tailwind configuration for improved styling and responsiveness

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,12 +5,26 @@ import {
   incrementByAmount,
 } from "../../store/slicers/counterSlicer";
 
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell} from "@heroui/react";
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@heroui/react";
+
+
 import { useTranslation } from "react-i18next";
 import { Button } from "@heroui/button";
 
 import { toast } from "react-toastify";
 import { addToast } from "@heroui/toast";
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+ 
+  useDisclosure,
+} from "@heroui/react";
 function Home() {
+  const {isOpen, onOpen, onOpenChange} = useDisclosure();
   const { t, i18n } = useTranslation();
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
@@ -27,7 +41,7 @@ function Home() {
         >
           +
         </button>
-        <button
+        <button 
           className="bg-red-500 text-white px-4 py-2 rounded"
           onClick={() => dispatch(decrement())}
         >
@@ -53,6 +67,9 @@ function Home() {
           <option value="fa">دری (Dari)</option>
         </select>
       </div>
+      <div className="flex gap-4">
+     
+    </div>
       <div className="my-5 flex items-center justify-center ">
         <Button
           onPress={() => {
@@ -67,7 +84,115 @@ function Home() {
         >
           Click me to show toast
         </Button>
+
+        
       </div>
+       <Button variant="bordered" radius="md">
+        Button
+      </Button>
+      <Button  color="secondary" radius="md">
+        Disabled
+      </Button>
+
+
+
+      <Table aria-label="Example static collection table">
+      <TableHeader>
+        <TableColumn>NAME</TableColumn>
+        <TableColumn>ROLE</TableColumn>
+        <TableColumn>STATUS</TableColumn>
+      </TableHeader>
+      <TableBody>
+        <TableRow key="1">
+          <TableCell>Tony Reichert</TableCell>
+          <TableCell>CEO</TableCell>
+          <TableCell>Active</TableCell>
+        </TableRow>
+        <TableRow key="2">
+          <TableCell>Zoey Lang</TableCell>
+          <TableCell>Technical Lead</TableCell>
+          <TableCell>Paused</TableCell>
+        </TableRow>
+        <TableRow key="3">
+          <TableCell>Jane Fisher</TableCell>
+          <TableCell>Senior Developer</TableCell>
+          <TableCell>Active</TableCell>
+        </TableRow>
+        <TableRow key="4">
+          <TableCell>William Howard</TableCell>
+          <TableCell>Community Manager</TableCell>
+          <TableCell>Vacation</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+<br /> <br />
+    <Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <Image
+          alt="heroui logo"
+          height={40}
+          radius="sm"
+          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
+          width={40}
+        />
+        <div className="flex flex-col">
+          <p className="text-md">HeroUI</p>
+          <p className="text-small text-default-500">heroui.com</p>
+        </div>
+      </CardHeader>
+      <Divider />
+      <CardBody>
+        <p>Make beautiful websites regardless of your design experience.</p>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <Link isExternal showAnchorIcon href="https://github.com/heroui-inc/heroui">
+          Visit source code on GitHub.
+        </Link>
+      </CardFooter>
+    </Card>
+
+    <br />
+
+    <>
+      <Button onPress={onOpen}>Open Modal</Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+              <ModalBody>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
+                  risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
+                  quam.
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
+                  risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
+                  quam.
+                </p>
+                <p>
+                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor
+                  adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit
+                  officia eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit incididunt
+                  nisi consectetur esse laborum eiusmod pariatur proident Lorem eiusmod et. Culpa
+                  deserunt nostrud ad veniam.
+                </p>
+              </ModalBody>
+              <ModalFooter>
+                <Button color="danger" variant="light" onPress={onClose}>
+                  Close
+                </Button>
+                <Button color="primary" onPress={onClose}>
+                  Action
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </>
     </div>
   );
 }
